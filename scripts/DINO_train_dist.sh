fc105ce2a87d8cc558c15fd3fad6c9a4e60270e5
yumu-173 <Liyichen173@163.com>
2023-03-27 21:35:05 +0800

    train with 4 layers

--- scripts/DINO_train_dist.sh
+++ scripts/DINO_train_dist.sh
@@ -4,7 +4,7 @@ GPUS=`nvidia-smi -L | wc -l`
 [[ -z "$RANK" ]] && RANK=0
 [[ -z "$AZUREML_NODE_COUNT" ]] && NODE_COUNT=1 || NODE_COUNT=$AZUREML_NODE_COUNT
 [[ -z "$AZ_BATCH_MASTER_NODE" ]] && MASTER_ADDR=127.0.0.1 || MASTER_ADDR=$(echo "$AZ_BATCH_MASTER_NODE" | cut -d : -f 1)
-[[ -z "$AZ_BATCH_MASTER_NODE" ]] && MASTER_PORT=44306 || MASTER_PORT=$(echo "$AZ_BATCH_MASTER_NODE" | cut -d : -f 2)
+[[ -z "$AZ_BATCH_MASTER_NODE" ]] && MASTER_PORT=44307 || MASTER_PORT=$(echo "$AZ_BATCH_MASTER_NODE" | cut -d : -f 2)
 
 echo "node rank: ${RANK}"
 echo "node count: ${NODE_COUNT}"
@@ -12,12 +12,14 @@ echo "master addr: ${MASTER_ADDR}"
 echo "master port: ${MASTER_PORT}"
 
 coco_path=$1
+config_path=$2
 python -m torch.distributed.run --nproc_per_node=${GPUS} \
     --nnodes ${NODE_COUNT} \
     --node_rank ${RANK} \
     --master_addr ${MASTER_ADDR} \
     --master_port ${MASTER_PORT} \
     main.py \
+    --config_file $config_path \
     --coco_path $coco_path \
     --rank ${RANK} \
     --n_nodes ${NODE_COUNT} \
